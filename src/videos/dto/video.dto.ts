8de6708d2d93e3d048ca005bde1a56b8dd25a6a9
Nickolay <arbuzov.n@f-case.ru>
2022-09-02 23:56:20 +0300

    fix

--- src/videos/dto/video.dto.ts
+++ src/videos/dto/video.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, Length, IsNumber, ValidateIf} from 'class-validator';
+import { IsString, Length, IsNumber, ValidateIf, IsBoolean} from 'class-validator';
 
 export class CreateVideoDto {
     @IsString()
@@ -7,6 +7,7 @@ export class CreateVideoDto {
     @IsString()
     @Length(1, 20)
     readonly author: string;
+    @IsBoolean()
     readonly canBeDownloaded: boolean;
     //@IsNumber()
     //@ValidateIf((object, value) => value !== null)
