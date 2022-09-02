f7435889c82318d092a437535e0d1ae7ddb3ac58
Nickolay <arbuzov.n@f-case.ru>
2022-09-02 21:06:26 +0300

    feat:

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function start() {
   const PORT = Number(process.env.PORT) || 5000
   const app = await NestFactory.create(AppModule, { cors: true })
+  app.useGlobalPipes(new ValidationPipe())
   //app.setGlobalPrefix('api')
   await app.listen(PORT, () => console.log(`NEST on ${PORT}`))
 }
