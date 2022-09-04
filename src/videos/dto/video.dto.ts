9fa37fc6390bd4909811d4d1250ee7d068e5e5bc
Nickolay <arbuzov.n@f-case.ru>
2022-09-05 01:27:03 +0300

    feat: update videodto

--- src/videos/dto/video.dto.ts
+++ src/videos/dto/video.dto.ts
@@ -1,31 +1,47 @@
-import { IsString, Length, IsNumber, ValidateIf, IsBoolean} from 'class-validator';
+import { IsString, Length, IsNumber, ValidateIf, IsBoolean, Min, Max, IsEnum, IsIn} from 'class-validator';
 
 export class CreateVideoDto {
+
     @IsString()
     @Length(1, 40)
     readonly title: string;
+
     @IsString()
     @Length(1, 20)
     readonly author: string;
+
     readonly canBeDownloaded: boolean;
-    //@IsNumber()
+
+    @IsNumber()
+    @Min(1)
+    @Max(18)
     //@ValidateIf((object, value) => value !== null)
     readonly minAgeRestriction: number | null;
+
+    @IsIn(['P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160'])
     readonly availableResolutions: string[];
 }
 
 export class UpdateVideoDto {
+
     @IsString()
     @Length(1, 40)
     readonly title: string;
+
     @IsString()
     @Length(1, 20)
     readonly author: string;
+
     @IsBoolean()
     readonly canBeDownloaded: boolean;
-    //@IsNumber()
+
+    @IsNumber()
+    @Min(1)
+    @Max(18)
     //@ValidateIf((object, value) => value !== null)
     readonly minAgeRestriction: number | null;
+
     readonly availableResolutions: string[];
+
     readonly publicationDate: string;
 }
