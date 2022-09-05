8c303b8df0f685114f3054f6eb54e7d1b916a130
Nickolay <arbuzov.n@f-case.ru>
2022-09-05 12:52:58 +0300

    fix

--- src/videos/dto/video.dto.ts
+++ src/videos/dto/video.dto.ts
@@ -1,14 +1,14 @@
 import { IsString, Length, IsNumber, ValidateIf, IsBoolean, Min, Max, IsEnum, IsIn, IsOptional} from 'class-validator';
 
 export enum availableResolutions {
-    P144,
-    P240,
-    P360,
-    P480,
-    P720,
-    P1080,
-    P1440,
-    P2160,
+    P144 = 'P144',
+    P240 = 'P240',
+    P360 = 'P360',
+    P480 = 'P480',
+    P720 = 'P720',
+    P1080 = 'P1080',
+    P1440 = 'P1440',
+    P2160 = 'P2160',
 }
 
 export class CreateVideoDto {
@@ -21,16 +21,17 @@ export class CreateVideoDto {
     @Length(1, 20)
     readonly author: string;
 
+    
     readonly canBeDownloaded: boolean;
 
     @IsOptional()
     @IsNumber()
     @Min(1)
     @Max(18)
-    @ValidateIf(value => value !== undefined)
+    //@ValidateIf(value => value !== undefined)
     readonly minAgeRestriction: number | undefined;
 
-    @IsEnum(availableResolutions)
+    @IsEnum(availableResolutions) //custom
     readonly availableResolutions: availableResolutions[];
 }
 
