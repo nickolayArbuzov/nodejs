b4b449040f4bf197d672820d4523536d4c63ee46
Nickolay <arbuzov.n@f-case.ru>
2022-09-06 01:28:06 +0300

    fix: remove regex in video validator

--- src/blogger/dto/blogger.dto.ts
+++ src/blogger/dto/blogger.dto.ts
@@ -7,7 +7,7 @@ export class CreateBloggerDto {
     
     @IsString()
     @Length(1, 100)
-    @Matches(/^https:([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
+    //@Matches(/^https:([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
     readonly url: string;
 }
 
