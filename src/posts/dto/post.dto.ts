7c5db4a9b23400e935069b796a23af34179f2412
Nickolay <arbuzov.n@f-case.ru>
2022-09-07 12:21:50 +0300

    fix

--- src/posts/dto/post.dto.ts
+++ src/posts/dto/post.dto.ts
@@ -13,6 +13,7 @@ export class CreatePostDto {
     @Length(1, 100)
     readonly shortDescription: string;
     
+    @Transform(({ value }: TransformFnParams) => value?.trim())
     @IsString()
     @Length(1, 1000)
     readonly content: string;
