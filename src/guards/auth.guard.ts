2e26b953959ef3fb488f738ef1489f16ffe2acf6
Nickolay <arbuzov.n@f-case.ru>
2022-09-07 15:19:25 +0300

    fix

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -7,7 +7,7 @@ export class AuthGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-    console.log(context)
+    console.log(request)
     return true;
   }
 }
