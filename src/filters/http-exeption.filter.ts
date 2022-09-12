f6fbfa0d43523001385eec5ec0951dbda23d74cf
Nickolay <arbuzov.n@f-case.ru>
2022-09-12 10:33:26 +0300

    feat

--- src/filters/http-exeption.filter.ts
+++ src/filters/http-exeption.filter.ts
@@ -11,7 +11,7 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const status = exception.getStatus();
 
     const res: any = exception.getResponse()
-    console.log('res', res)
+    console.log('exception', exception)
     if (status === 400) {
       const errorResponse = {
         errorsMessages: [],
