c5958fd3db7eb4592d2b5e2454d7c170fa5e0107
Nickolay <arbuzov.n@f-case.ru>
2022-09-14 17:21:05 +0300

    feat

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -39,16 +39,12 @@ export class UserService {
     //TODO: automapper
     //TODO: property order in returned obj's
     const returnedUsers = all.map(a => {return {createdAt: a.createdAt, email: a.email, id: a.id, login: a.login}})
-    const alluser = await this.userRepository.find()
-    console.log('alluser', alluser)
-    console.log('returnedUsers', returnedUsers)
-    console.log('user-query', query)
     return {
       pagesCount: Math.ceil(count/(query.pageSize ? + +query.pageSize : +queryDefault.pageSize)), 
       page: query.pageNumber ? +query.pageNumber : +queryDefault.pageNumber, 
       pageSize: query.pageSize ? +query.pageSize : +queryDefault.pageSize, 
       totalCount: count, 
-      items: query.sortBy === 'name' ? returnedUsers.sort((a,b) => a.login > b.login && sortDirection === 'ASC' ? 1 : -1 ) : returnedUsers
+      items: query.sortBy === 'login' ? returnedUsers.sort((a,b) => a.login > b.login && sortDirection === 'ASC' ? 1 : -1 ) : returnedUsers
     }
     
   }
