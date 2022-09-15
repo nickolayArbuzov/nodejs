44388420d4116835f2ef213b6eb8c252385fa7ec
Nickolay <arbuzov.n@f-case.ru>
2022-09-15 19:22:34 +0300

    feat

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,19 +2,21 @@ import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { User } from '../users/user.entity';
 import { Repository } from 'typeorm';
 import { AuthDto } from './dto/auth.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @Inject('USER_REPOSITORY')
     private readonly userRepository: Repository<User>,
+    private jwtService: JwtService
   ) {}
 
   async login(authDto: AuthDto) {
-    const auth = await this.userRepository.findOne({where: {login: authDto.login, password: authDto.password}})
-    //jwt
+    const auth: User = await this.userRepository.findOne({where: {login: authDto.login, password: authDto.password}})
     if (auth) {
-      return auth
+      const payload = {email: auth.email, login: auth.login}
+      return {token: this.jwtService.sign(payload)}
     } 
     else {
       throw new HttpException('Auth not found', HttpStatus.UNAUTHORIZED);
