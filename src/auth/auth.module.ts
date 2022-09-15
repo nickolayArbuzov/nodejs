94c97b260f691308adc15903002cb475d96d4790
Nickolay <arbuzov.n@f-case.ru>
2022-09-15 21:06:48 +0300

    feat

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '../users/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
   controllers: [AuthController],
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: process.env.PRIVATE_KEY || 'SECRET',
+      signOptions: {
+        expiresIn: '24h'
+      }
+    })
+  ],
   providers: [
     AuthService,
   ],
+  exports: [JwtModule]
 })
 export class AuthModule {}
