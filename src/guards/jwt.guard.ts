dfdcc38e29b497d79c0b55323f408e05992177bf
Nickolay <arbuzov.n@f-case.ru>
2022-09-16 23:34:46 +0300

    feat

--- src/guards/jwt.guard.ts
+++ src/guards/jwt.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { SimpleConsoleLogger } from 'typeorm';
 
 @Injectable()
 export class JWTGuard implements CanActivate {
@@ -12,6 +13,7 @@ export class JWTGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean {
     const request: Request = context.switchToHttp().getRequest();
+    console.log('request.headers?.authorization', request.headers?.authorization)
     const user = this.jwtService.verify(request.headers?.authorization?.split(' ')[1])
     console.log('user-jwt', user)
     if (user){
