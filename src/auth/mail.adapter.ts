a14b4a57651173105449cac31e39ba4e464af035
Nickolay <arbuzov.n@f-case.ru>
2022-09-18 14:05:29 +0300

    feat

--- src/auth/mail.adapter.ts
+++ src/auth/mail.adapter.ts
@@ -12,13 +12,15 @@ export const sendEmail = async (email: string, code: string) => {
     }
   })
 
+  const emailTemplate = (code: string) => `<h1>Thank for your registration</h1>
+      <p>To finish registration please follow the link below:
+        <a href='https://somesite.com/confirm-email?code=${code}'>complete registration</a>
+     </p>`
+
   await transporter.sendMail({
     from: process.env.GMAIL_ACCAUNT,
     to: email,
-    html: `<h1>Thank for your registration</h1>
-      <p>To finish registration please follow the link below:
-         <a hreh='https://somesite.com/confirm-email?code=${code}'>complete registration</a>
-     </p>`,
+    html: emailTemplate(code),
     subject: 'Регистрация',
   })
 
