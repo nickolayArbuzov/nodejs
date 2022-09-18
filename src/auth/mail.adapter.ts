76cc8cba275ccc1ddd66aeed8ee8a08bae4fce0e
Nickolay <arbuzov.n@f-case.ru>
2022-09-18 14:24:20 +0300

    feat

--- src/auth/mail.adapter.ts
+++ src/auth/mail.adapter.ts
@@ -14,7 +14,7 @@ export const sendEmail = async (email: string, code: string) => {
 
   const emailTemplate = (code: string) => `<h1>Thank for your registration</h1>
       <p>To finish registration please follow the link below:
-        <a hreh='https://somesite.com/confirm-email?code=${code}'>complete registration</a>
+        <a href='https://somesite.com/confirm-email?code=${code}'>complete registration</a>
      </p>`
 
   await transporter.sendMail({
